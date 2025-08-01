03cb7449f857cf3ea70077f8426fbfe2cfcc6d45
Berat YILGIN <berat2002ylgn@gmail.com>
2025-08-01 13:53:26 +0300

    Customer modülü için temel yapı eklendi: - CustomerController oluşturuldu - Views/Customer klasörü ve Index sayfası eklendi - User.cs model SQL tablo alanlarına uyarlandı - Menüye Müşteriler linki eklendi

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EFaturaApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: /Customer/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,15 +1,30 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFaturaApp.Models
 {
+    [Table("USERS")] // Tablo adı
     public class User
     {
+        [Key]
         public int Id { get; set; }
+
+        [Column("USERNAME")]
         public string Username { get; set; }
-        public string Password { get; set; }  // ileride hash yapılacak
+
+        [Column("PASSWORD_")] // SQL'deki adı PASSWORD_
+        public string Password { get; set; }
+
+        [Column("FULLNAME")] // SQL'de FULLNAME varsa
         public string Fullname { get; set; }
+
+        [Column("EMAIL")] // SQL'deki adı EMAIL
         public string Email { get; set; }
+
+        [Column("TELNO")] // SQL'deki adı TELNO
         public string Telno { get; set; }
+
+        [Column("CREATEDATE")]
         public DateTime CreateDate { get; set; }
     }
 }
