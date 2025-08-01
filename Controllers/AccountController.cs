f4db20287e52ff0b7b569eb1c2e48f0184a5bb06
Berat YILGIN <berat2002ylgn@gmail.com>
2025-08-01 12:55:54 +0300

    Ana Dosyalar oluşturuldu.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EFaturaApp.Data;
+using EFaturaApp.Models;
+using System.Linq;
+
+namespace EFaturaApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly EFaturaContext _context;
+
+        public AccountController(EFaturaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+
+            if (user != null)
+            {
+                TempData["Message"] = $"Welcome {user.Fullname}!";
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Invalid username or password!";
+            return View();
+        }
+
+        // GET: Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            user.CreateDate = DateTime.Now;
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EFaturaApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }  // ileride hash yapılacak
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public string Telno { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
