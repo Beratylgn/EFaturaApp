d8e33d75f6980d5ec9fe27d383d872e4ff40a8b1
Berat YILGIN <berat2002ylgn@gmail.com>
2025-08-04 12:33:52 +0300

    feat(customer): Müşteri modülüne Düzenle ve Sil işlemleri eklendi

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -40,5 +40,69 @@ namespace EFaturaApp.Controllers
             }
             return View(customer);
         }
+
+        // GET: /Customer/Edit/5
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var customer = _context.Customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: /Customer/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingCustomer = _context.Customers.FirstOrDefault(c => c.Id == customer.Id);
+                if (existingCustomer == null)
+                {
+                    return NotFound();
+                }
+
+                // Sadece değişen alanları güncelle
+                existingCustomer.CompanyName = customer.CompanyName;
+                existingCustomer.TaxNumber = customer.TaxNumber;
+                existingCustomer.Address = customer.Address;
+                existingCustomer.Email = customer.Email;
+
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+
+        // GET: /Customer/Delete/5
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var customer = _context.Customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: /Customer/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var customer = _context.Customers.FirstOrDefault(c => c.Id == id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
